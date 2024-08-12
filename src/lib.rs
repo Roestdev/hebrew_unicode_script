25ae70985556ad73e2ac1c9eb3786f331a0db094
roest_dev <github.ebook242@passinbox.com>
2024-08-12 13:14:40 +0200

    feat: one unique path to all functions
    In previous releases is was possible to use a long and
    a short path to a function. This is no longer possible.
    e.g. use hebrew_unicode_script::function_api::unicode_script_hebrew::is_hbr_script;
    is no longer supported!
    
    use hebrew_unicode_script::is_hbr_script; is the way to go.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 
-pub mod function_api;
+mod function_api;
 
 pub use self::function_api::unicode_block_alphabetic_presentation_form::*;
 pub use self::function_api::unicode_block_hebrew::*;
